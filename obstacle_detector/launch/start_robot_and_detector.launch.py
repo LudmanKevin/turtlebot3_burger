834efb9be2c24549466c75500840ab1623fa9fdc
Ludmán Kevin <ludmankevin@gmail.com>
2025-12-05 12:07:28 +0100

    launch file fix

--- obstacle_detector/launch/start_robot_and_detector.launch.py
+++ obstacle_detector/launch/start_robot_and_detector.launch.py
@@ -6,29 +6,30 @@ from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    # turtlebot3_bringup csomag launch könyvtára
     bringup_launch_file = os.path.join(
         get_package_share_directory('turtlebot3_bringup'),
         'launch',
         'robot.launch.py'
     )
 
+    ekf_launch_file = os.path.join(
+        get_package_share_directory('tb3_localization'),
+        'launch',
+        'ekf_launch.py'
+    )
+
     return LaunchDescription([
-        # TurtleBot3 bringup indítása
+        # TurtleBot3 bringup
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(bringup_launch_file)
         ),
 
-        # tb3_localization node indítása
-        Node(
-            package='robot_localization',
-            executable='ekf_node',
-            output='screen',
-            name='ekf_filter_node',
-            parameters=['/home/arnl/ros2_ws/src/turtlebot3_burger_csomag/tb3_localization/config/ekf.yaml']
+        # EKF localization (tb3_localization package-ből)
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(ekf_launch_file)
         ),
 
-        # obstacle_detector node indítása
+        # obstacle_detector node
         Node(
             package='obstacle_detector',
             executable='obstacle_detector',
